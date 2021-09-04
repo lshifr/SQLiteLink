e63ada79bf6ea95fa1a20788ad61e07676d94872
Leonid Shifrin <leonids@wolfram.com>
2021-09-04 15:12:06 +0300

    Updated multiple connections API

--- Libraries/SQLiteLink/Common/connections.c
+++ Libraries/SQLiteLink/Common/connections.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "common.h"
+#include "single_connection.h"
 #include "connections.h"
 
 
@@ -16,46 +17,12 @@ static int get_first_available_connection_index() {
 }
 
 
-static void init_connection(connection_info* conn, const char* path) {
-    if (conn->in_use) {
-        return; // Can only init unused connection
-    }
-    conn->file_path = str_dup(path); // TODO: handle NULL pointer from str_dup
-    conn->in_use = TRUE;
-    conn->connection = NULL;
-    conn->sqlite_callback_data = NULL;
-    conn->sql = NULL;
-    conn->sqliteErrMsg = NULL;
-}
-
-
-static void refresh_connection_info(connection_info* cinfo) {
-    if (cinfo->serialized_string) {
-        free((void*)cinfo->serialized_string);
-        cinfo->serialized_string = NULL;
-    }
-    if (cinfo->sqliteErrMsg) {
-        free((void*)cinfo->sqliteErrMsg);
-        cinfo->sqliteErrMsg = NULL;
-    }
-    if (cinfo->sql) {
-        free((void*)cinfo->sql);
-        cinfo->sql = NULL;
-    }
-}
-
-
-static int is_connected_unchecked(connection_info* conn) {
-    return conn->connection != NULL;
-}
-
-
 int new_connection(const char* path) {
     int index = get_first_available_connection_index();
     if (index == MAX_CONNECTIONS_EXCEEDED) {
         return MAX_CONNECTIONS_EXCEEDED;
     }
-    init_connection(&(connections[index]), path);
+    sqlite_connection_create(&(connections[index]), path);
     return index;
 }
 
@@ -70,7 +37,7 @@ connection_info* get_connection_info(int cindex) {
 
 BOOL connection_exists(int cindex) {
     connection_info* conn_info = get_connection_info(cindex);
-    return conn_info && conn_info->in_use;
+    return conn_info && sqlite_is_connection_in_use(conn_info);
 }
 
 
@@ -81,12 +48,7 @@ int connect(int cindex) {
         return INVALID_CONNECTION_INDEX;
     }
     conn_info = get_connection_info(cindex);
-    assert(conn_info->file_path);
-    if (is_connected_unchecked(conn_info)) {
-        return SQLITE_SUCCESS; // If already connected, do nothing
-    }
-    result = sqlite3_open(conn_info->file_path, &(conn_info->connection));
-    return result == SQLITE_OK ? SQLITE_SUCCESS : SQLITE_CONNECTION_FAILURE;
+    return sqlite_connect(conn_info) == SQLITE_SUCCESS? SUCCESS : FAILURE;
 }
 
 
@@ -97,141 +59,25 @@ int disconnect(int cindex) {
         return INVALID_CONNECTION_INDEX;
     }
     conn_info = get_connection_info(cindex);
-    if (!is_connected_unchecked(conn_info)) {
-        return SQLITE_SUCCESS; // If already disconnected, do nothing
-    }
-    result = sqlite3_close(conn_info->connection);
-    if (result == SQLITE_OK) {
-        conn_info->connection = NULL;
-        return SQLITE_SUCCESS;
-    }
-    return SQLITE_DISCONNECTION_FAILURE;
+    return sqlite_disconnect(conn_info) == SQLITE_SUCCESS ? SUCCESS : FAILURE;
 }
 
 
 int destroy_connection(int cindex) {
     connection_info* conn_info;
-    int result;
+    sqlite_rcode result;
     if (!connection_exists(cindex)) {
         return INVALID_CONNECTION_INDEX;
     }
     conn_info = get_connection_info(cindex);
-    result = disconnect(cindex); // Just in case
+    result = sqlite_disconnect(conn_info); // Just in case
     if (result != SQLITE_SUCCESS) {
-        return result;
+        return FAILURE;
     }
-    refresh_connection_info(conn_info);
-    if (conn_info->file_path) {
-        free((void*)conn_info->file_path);
-        conn_info->file_path = NULL;
-    }
-    conn_info->in_use = FALSE;
-    return SQLITE_SUCCESS;
+    return sqlite_connection_destroy(conn_info) == SQLITE_SUCCESS ? SUCCESS : FAILURE;
 }
 
 
 BOOL is_connected(int cindex) {
-    return connection_exists(cindex) && is_connected_unchecked(get_connection_info(cindex));
-}
-
-
-// const char* get_connection_info_JSON_string(int index);
-
-
-const char* get_sqlite_error_message(connection_info* cinfo) {
-    return cinfo->sqliteErrMsg;
-}
-
-/*
-**  A wrapper around sqlite3_exec, that takes care of error message memory
-**  management, and adds a few debugging print statements.
-**
-**  To execute this on its own, one has to fill in sql string and callback data,
-**  into the passed <connection_info> structure, prior to this call.
-*/
-int execute_sql(connection_info* cinfo, int cb(void*, int, char**, char**)) {
-    char* zErrMsg = NULL;
-    if (!cinfo->connection) {
-        // TODO: This should not actually happen. assert() might be better here.
-        return SQLITE_CONNECTION_DISCONNECTED;
-    }
-    DEBUG_STMT(printf(
-        "About to execute SQL statement: \n%s\n, for connection %s\n",
-        cinfo->sql,
-        cinfo->file_path
-    ));
-    int result = sqlite3_exec(
-        cinfo->connection,
-        cinfo->sql,
-        cb,
-        cinfo->sqlite_callback_data,
-        &zErrMsg
-    );
-    if (result != SQLITE_OK) {
-        DEBUG_STMT(fprintf(stderr, "SQL error: %s\n", zErrMsg));
-        if (zErrMsg) {
-            cinfo->sqliteErrMsg = str_dup(zErrMsg);  // TODO: handle NULL pointer from str_dup
-            sqlite3_free(zErrMsg);
-        }
-    }
-    else {
-        if (cinfo->sqliteErrMsg) {
-            free((void*)cinfo->sqliteErrMsg);
-            cinfo->sqliteErrMsg = NULL;
-        }
-    }
-    return result == SQLITE_OK ? SQLITE_SUCCESS : SQLITE_EXECUTION_FAILURE;
-}
-
-
-static void set_sql_string(connection_info* cinfo, const char* sql) {
-    if (cinfo->sql) {
-        free((void*)cinfo->sql);
-    }
-    cinfo->sql = str_dup(sql); // TODO: handle NULL pointer from str_dup
-}
-
-
-static void set_sqlite_callback_data(void* cinfo, void* cbdata) {
-    ((connection_info*)cinfo)->sqlite_callback_data = cbdata;
-}
-
-
-static int exec_sql_for_serialization(void* exec_data, int cb(void*, int, char**, char**)) {
-    return execute_sql((connection_info*)exec_data, cb);
-}
-
-/*
-**  This executes SQL with a given connection, and serializes the result to string.
-**  The serializer function should be passed as a third argument. It can use the
-**  passed callback data setter (set_sqlite_callback_data here), on exec_data
-**  (which is a pointer to connection data, in disguise), to set whatever callback
-**  information it will need. It is supposed to call <exec> (<exec_sql_for_serialization>
-**  here), somewhere in its body, providing its own custom callback function, which
-**  will be called inside <exec> for each data row. That custom callback function
-**  call allows serializer to collect the necessary info for that row serialization,
-**  adding it to the callback data field that it has supposedly previously set.
-*/
-int execute_sql_with_serialization(
-    connection_info* cinfo,
-    const char* sql,
-    int serializer(
-        void* exec_data,
-        void set_callback_data(void* exec_data, void* callback_data),
-        int exec(
-            void* exec_data,
-            int cb(void*, int, char**, char**)
-        ),
-        const char** result_ptr
-    )
-)
-{
-    refresh_connection_info(cinfo);
-    set_sql_string(cinfo, sql);
-    return serializer(
-        (void*)cinfo,
-        set_sqlite_callback_data,
-        exec_sql_for_serialization,
-        &(cinfo->serialized_string)
-    );
+    return connection_exists(cindex) && sqlite_is_connected(get_connection_info(cindex));
 }
--- Libraries/SQLiteLink/Common/connections.h
+++ Libraries/SQLiteLink/Common/connections.h
@@ -2,27 +2,12 @@
 #define CONNECTIONS_INCLUDED
 
 #include "common.h"
+#include "single_connection.h"
 
-#include <sqlite3.h>
-
-typedef struct connection_info {
-    const char* serialized_string;
-    const char* sqliteErrMsg;
-    const char* file_path;
-    sqlite3* connection;
-    const char* sql;
-    void* sqlite_callback_data;
-    BOOL in_use;
-} connection_info;
 
 #define MAX_ACTIVE_CONNECTIONS 1000
-#define SQLITE_SUCCESS 0
 #define MAX_CONNECTIONS_EXCEEDED -1
-#define INVALID_CONNECTION_INDEX 1
-#define SQLITE_CONNECTION_FAILURE 2
-#define SQLITE_DISCONNECTION_FAILURE 3
-#define SQLITE_EXECUTION_FAILURE 4
-#define SQLITE_CONNECTION_DISCONNECTED 5
+#define INVALID_CONNECTION_INDEX 2
 
 
 int new_connection(const char* path);
@@ -33,22 +18,5 @@ int disconnect(int cindex);
 BOOL is_connected(int cindex);
 connection_info* get_connection_info(int cindex);
 
-const char* get_sqlite_error_message(connection_info* cinfo);
-
-int execute_sql(connection_info* cinfo, int cb(void*, int, char**, char**));
-
-int execute_sql_with_serialization(
-    connection_info* cinfo,
-    const char* sql,
-    int serializer(
-        void* exec_data,
-        void set_callback_data(void* exec_data, void* callback_data),
-        int exec(
-            void* exec_data,
-            int cb(void*, int, char**, char**)
-        ),
-        const char** result_ptr
-    )
-);
 
 #endif
