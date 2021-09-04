311e2817aeda66e953c058a4bd4fbe1a2a6397e3
Leonid Shifrin <leonids@wolfram.com>
2021-09-04 10:02:10 +0300

    Add struct field setter macro for dynamically allocated fields

--- Libraries/SQLiteLink/Common/common.h
+++ Libraries/SQLiteLink/Common/common.h
@@ -1,6 +1,8 @@
 #ifndef COMMON_INCLUDED
 #define COMMON_INCLUDED
 
+#include <stdlib.h>
+
 #define TRUE 1
 #define FALSE 0
 #define BOOL int
@@ -16,6 +18,14 @@
     }\
 }\
 while(0)
+
+#define SET_DALLOC_POINTER_FIELD(pointer, field, value)     \
+do {                                                        \
+    if ((pointer)->field) {                                 \
+        free((void*)(pointer)->field);                      \
+    }                                                       \
+    (pointer)->field = (value);                             \
+} while(0)  
 
 char* str_dup(const char* src);
 
